f0bfc2742abedebed45de310c6bb0f907cc37962
Annshix <sx985452080@gmail.com>
2016-12-08 13:46:04 +0800

    v-1.0

--- segment.js
+++ segment.js
@@ -1,7 +1,7 @@
 /**
  * Created by apple on 16/11/22.
  */
-function segment(width, height, color){
+function Segment(width, height, color){
   this.x = 0;
   this.y = 0;
   this.width = width;
@@ -15,15 +15,51 @@ function segment(width, height, color){
   this.lineWidth = 1;
 }
 
-segment.prototype.draw(context){
+Segment.prototype.draw(context){
   var h = this.height,
     d = this.width + h,
     r = h/2;    //leg radius
   context.save();
   context.translate(this.x, this.y);
   context.rotate(this.rotation);
+  context.scale(this.scaleX, this.scaleY);
   context.lineWidth = this.lineWidth;
   context.fillStyle = this.color;
-  context.fillTRect(this.x, this.y, this.x + d, this.y + h);
 
+  context.beginPath();
+  context.moveTo(0, -r);
+  context.lineTo(d-2*r, -r);
+  context.quadraticCurveTo(d-r, -r, d-r, 0);  //Bézier curve
+  context.lineTo(d-r,h-2*r);
+  context.quadraticCurveTo(d-r, h-r, d-2*r, h-r);
+  context.lineTo(0, h-r);
+  context.quadraticCurveTo(-r, h-r, -r, h-2*r);
+  context.lineTo(-r, 0);
+  context.quadraticCurveTo(-r, -r, 0, -r);
+  context.closePath();
+
+  context.fill();
+  if(this.lineWidth > 0){
+    context.stroke();
+  }
+
+  context.beginPath();
+  context.arc(0,0,2,0,(MATH.PI * 2),true);
+  context.closePath();
+  context.stroke();
+
+  context.beginPath();
+  context.arc(this.width,0,2,0,(MATH.PI * 2),true);
+  context.closePath();
+  context.stroke();
+
+  context.restore();
 }
+
+Segment.prototype.getPin = function(){
+  return{
+    x: this.x + Math.cos(this.rotation) * this.width,
+    y: this.y + Math.sin(this.rotation) * this.width
+  };
+};
+
